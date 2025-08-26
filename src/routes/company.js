a37c8a9f0a4fd5ad354c8668de61dd74633ec55d
Felipe Jeldes Ramirez <felipejeldesramirez@MacBook-Pro-de-Felipe.local>
2025-08-25 23:07:49 -0600

    WIP

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Company = require('../models/Company');
+const Company = require('../models/company');
 
 const router = express.Router();
 
--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Transaction = require('../models/Transaction');
+const Transaction = require('../models/transaction');
 const User = require('../models/User');
 const Product = require('../models/Product');
 
