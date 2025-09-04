9399be56b2d78cdf152ac0dbd0680a97c500011f
Felipe Jeldes Ramirez <felipejeldesramirez@MacBook-Pro-de-Felipe.local>
2025-09-04 13:15:25 -0600

    WIP

--- src/models/user.js
+++ src/models/user.js
@@ -1,53 +1,75 @@
 // models/User.js
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../../db'); // 👈 ajusta la ruta según tu proyecto
+const { sequelize } = require('../../db'); // ajusta la ruta según tu proyecto
 
-  const User = sequelize.define('User',
-  {
-    id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING(150),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(150),
-      allowNull: false,
-      unique: true,
-    },
-    passwordHash: {   
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      field: 'password_hash'
-    },
-    salt: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    role: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      defaultValue: 'user',
-    },
-    isActive: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-    lastLogin: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+    field: 'id',
   },
-  {
-    sequelize,
-    tableName: 'users',
-    modelName: 'User',
-    timestamps: true,
-  }
-);
+  uuid: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+    field: 'uuid',
+  },
+  name: {
+    type: DataTypes.STRING(150),
+    allowNull: false,
+    field: 'name',
+  },
+  email: {
+    type: DataTypes.STRING(150),
+    allowNull: false,
+    unique: true,
+    field: 'email',
+  },
+  passwordHash: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    field: 'password_hash',
+  },
+  salt: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+    field: 'salt',
+  },
+  role: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    defaultValue: 'user',
+    field: 'role',
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+    field: 'is_active',
+  },
+  lastLogin: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    field: 'last_login',
+  },
+  createdBy: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    field: 'created_by',
+  },
+  updatedBy: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    field: 'updated_by',
+  },
+}, {
+  sequelize,
+  tableName: 'users',
+  modelName: 'User',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+});
 
 module.exports = User;
-
